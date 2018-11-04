2b2dacf595abbe43bfbe82eb4cfefdd2da7aa836
Tácio Nery <taiconery@gmail.com>
2018-11-04 12:58:12 -0300

    Get data from ipca website using Scrapy

--- ipca_spider.py
+++ ipca_spider.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
 import scrapy
+import csv
 
 class IPCASpider(scrapy.Spider):
     name = 'ipcaspider'
     start_urls = ['http://www.debit.com.br/consulta30.php?indice=ipca']
 
     def parse(self, response):
-        for item in response.css('.listagem .zb'):
-            obj = { 'yeah_month': item.css('.ta-left ::text').extract_first(),
+        ipca_indexes = list()
+        for item in response.css('.listagem > tbody > tr'):
+            obj = { 'year_month': item.css('.ta-left ::text').extract_first(),
                     'ipca': item.css('.ta-right ::text').extract_first() }
 
-            yield obj
-            # TODO write CSV
+            ipca_indexes.append(obj)
+
+        with open('./ipca.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file, delimiter=';')
+            for idx in ipca_indexes:
+                print(idx)
+                line = [idx['year_month'].replace("/", "-"), idx['ipca'].replace(",", ".")]
+                print(line)
+                writer.writerow(line)
